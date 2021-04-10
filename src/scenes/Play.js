691d011e29e11e8a89a4d7cc88034fe91bf3f45f
Liam Booher <lnbooher@ucsc.edu>
2021-04-10 16:51:52 -0700

    Rocketship added and movement created.

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,11 +6,15 @@ class Play extends Phaser.Scene {
 
     preload(){
         this.load.image('starfield', 'Assets/starfield.png');
+        this.load.image('rocket', 'Assets/rocket.png');
     }
 
     create(){
         //Starfield sprite
         this.starfield = this.add.tileSprite(0,0,640,480, 'starfield').setOrigin(0,0);
+        //Rocket sprite
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5, 0);
+
         //green ui background
         this.add.rectangle(0,
             borderUISize + borderPadding,
@@ -18,14 +22,22 @@ class Play extends Phaser.Scene {
             borderUISize * 2,
             0x00FF00,
         ).setOrigin(0,0);
+
         //White borders
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    
+        // define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update(){
         this.starfield.tilePositionX -=4;
+        this.p1Rocket.update();
     }
 }
